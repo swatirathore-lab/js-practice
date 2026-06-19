39c080d3bc0e42056825f9a09b3e4bd94499fdfe
swatirathore-lab <swatirathore683@gmail.com>
2026-06-19 08:55:26 +0530

    adding dom and it's properties related codes

---
+++ jspart9/app.js
@@ -0,0 +1,13 @@
+//Selecting element by classname
+// let smallImages = document.getElementsByClassName("oldImg");
+// for(let i=0;i< smallImages.length;i++){
+//     smallImages[i].src="assets/spiderman_img.png";
+//     console.log(`value of image no. ${i} is changed`);
+
+// }
+//Query Selector
+console.dir(document.querySelector("h1"));
+console.dir(document.querySelector("#description"));
+console.dir(document.querySelector(".oldImg"));
+console.dir(document.querySelector("div a"));
+console.dir(document.querySelectorAll("div a"));
